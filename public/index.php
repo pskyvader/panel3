948461bd2a8e18f564304ce5a9071ee705a2b6c2
pskyvader <pskyvader@hotmail.com>
2019-01-11 13:20:25 -0600

    a

--- public/index.php
+++ public/index.php
@@ -11,11 +11,19 @@ function autoload_classes($class_name)
     $filename = PROJECTPATH . '/' . str_replace('\\', '/', $class_name) . '.php';
     if (is_file($filename)) {
         include_once $filename;
-    }else{
-        $filename=strtolower($filename);
-        if (is_file($filename)) {
-            include_once $filename;
-        }else{
+    } else {
+        $lowerfile = strtolower($filename);
+        $include='';
+        foreach (glob(dirname($filename) . '/*') as $file) {
+            if (strtolower($file) == $lowerfile) {
+                $include=$file;
+                break;
+            }
+        }
+
+        if (is_file($include)) {
+            include_once $include;
+        } else {
             var_dump($filename);
         }
     }
