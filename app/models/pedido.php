598396f1654f5d8242e41738ff9fac3b513c3bca
mysitioc <mysitioc@mysitio.cl>
2019-01-24 11:06:44 -0600

    --- Auto-Git Commit ---

--- app/models/pedido.php
+++ app/models/pedido.php
@@ -62,6 +62,39 @@ class pedido extends base_model
         return $row;
     }
 
+    public static function copy(int $id)
+    {
+        $row = static::getById($id);
+        if (isset($row['foto'])) {
+            $foto_copy=$row['foto'];
+            unset($row['foto']);
+        }
+        if (isset($row['archivo'])) {
+            unset($row['archivo']);
+        }
+        $fields     = table::getByname(static::$table);
+        $insert     = database::create_data($fields, $row);
+        $connection = database::instance();
+        $row        = $connection->insert(static::$table, static::$idname, $insert, self::$delete_cache);
+        if (is_int($row) && $row>0) {
+            $last_id = $row;
+            if(isset($foto_copy)){
+                $new_fotos=array();
+                foreach ($foto_copy as $key => $foto) {
+                    $copiar = image::copy($foto, $last_id, $foto['folder'], $foto['subfolder'], $last_id, '');
+                    $new_fotos[]=$copiar['file'][0];
+                    image::regenerar($copiar['file'][0]);
+                }
+                $update=array('id'=>$last_id,'foto'=>functions::encode_json($new_fotos));
+                static::update($update);
+            }
+            log::insert_log(static::$table, static::$idname, __FUNCTION__, $insert);
+            return $last_id;
+        } else {
+            return $row;
+        }
+    }
+
     public static function getByCookie(string $cookie, bool $estado_carro = true)
     {
         $where = array("cookie_pedido" => $cookie);
