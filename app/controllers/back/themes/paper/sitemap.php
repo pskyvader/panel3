d9ee05ef8b3a916ccc66b37f1debf2185a7c707a
mysitioc <mysitioc@mysitio.cl>
2019-03-18 12:10:46 -0500

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/sitemap.php
+++ app/controllers/back/themes/paper/sitemap.php
@@ -16,7 +16,7 @@ class sitemap extends base
     protected $breadcrumb = array();
     public function __construct()
     {
-        parent::__construct(sitemap_model);
+        parent::__construct(new sitemap_model());
     }
     public function index()
     {
