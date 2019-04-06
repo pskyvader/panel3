9860be445b5fe1c4f9eb77600981e8644a9dfbf3
mysitioc <mysitioc@mysitio.cl>
2019-04-05 19:44:31 -0500

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/aside.php
+++ app/controllers/back/themes/paper/aside.php
@@ -141,7 +141,9 @@ class aside
             view::set('name', $administrador["nombre"]);
             view::set('email', $administrador["email"]);
             view::set('url_admin', functions::generar_url(array("administrador", "detail", $administrador[0], 'profile'), array('tipo' => $tipo_admin)));
-            view::set('img_admin', image::generar_url($administrador["foto"][0], 'profile'));
+            if(is_array($administrador["foto"])){
+                view::set('img_admin', image::generar_url($administrador["foto"][0], 'profile'));
+            }
 
 
             view::render('aside');
