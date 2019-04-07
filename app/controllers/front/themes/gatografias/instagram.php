3a7f4cfd3e045e548cff45601fb492ff093226e5
mysitioc <mysitioc@mysitio.cl>
2019-04-07 16:05:04 -0500

    --- Auto-Git Commit ---

--- app/controllers/front/themes/gatografias/instagram.php
+++ app/controllers/front/themes/gatografias/instagram.php
@@ -1,7 +1,8 @@
 <?php
-namespace app\controllers\front\themes\gatografias;
+namespace app\controllers\front\themes\focus;
 
 defined("APPPATH") or die("Acceso denegado");
+use \app\models\configuracion as configuracion_model;
 use \core\app;
 use \core\functions;
 
@@ -14,16 +15,16 @@ class instagram
     {
         $config          = app::getConfig();
         $instagram_token = $config['instagram_token'];
-        $photo_count     = 8;
+        $photo_count     = configuracion_model::getByVariable('instagram_cantidad', 8);
         $json_link       = "https://api.instagram.com/v1/users/self/media/recent/?";
         $json_link .= http_build_query(array('access_token' => $instagram_token, 'count' => $photo_count));
-        $json = functions::decode_json(functions::url_get_contents($json_link));
-        $respuesta=array();
+        $json      = functions::decode_json(functions::url_get_contents($json_link));
+        $respuesta = array();
         foreach ($json['data'] as $key => $data) {
-            $respuesta[]=array(
-                'url'=>$data['link'],
-                'title'=>$data['caption']['text'],
-                'images'=>$data['images'],
+            $respuesta[] = array(
+                'url'    => $data['link'],
+                'title'  => $data['caption']['text'],
+                'images' => $data['images'],
             );
         }
         echo functions::encode_json($respuesta);
