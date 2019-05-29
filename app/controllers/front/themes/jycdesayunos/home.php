aa27a7c5ee453cbc2a910b7c94577286594c718f
Pablo <pskyvader@hotmail.com>
2019-05-29 13:10:12 -0500

    --- Auto-Git Commit ---

--- app/controllers/front/themes/jycdesayunos/home.php
+++ app/controllers/front/themes/jycdesayunos/home.php
@@ -34,8 +34,6 @@ class home extends base
         //$breadcrumb = new breadcrumb();
         //$breadcrumb->normal($this->breadcrumb);
 
-        echo "asdfadsf";
-
         $secciones_destacadas = seccion_model::getAll(array('tipo' => 3, 'destacado' => true));
         $seo                  = seo::getById(7);
         foreach ($secciones_destacadas as $key => $seccion) {
@@ -47,9 +45,9 @@ class home extends base
             view::render('home-text');
         }
 
-        
-        echo "asdfadsf";
+        echo "asdf";
         $productos_destacados = producto_model::getAll(array('tipo' => 1, 'destacado' => true),array('limit'=>6));
+        echo "asdf";
         if (count($productos_destacados)>0) {
             //$seo_productos          = seo::getById(8);
             //$this->url[0] = $seo_productos['url'];
@@ -65,8 +63,8 @@ class home extends base
             view::set('title', "Nuestros productos destacados");
             view::render('home-products');
         }
+        echo "ASDF";
 
-        echo "asdfadsf";
         $footer = new footer();
         $footer->normal();
     }
